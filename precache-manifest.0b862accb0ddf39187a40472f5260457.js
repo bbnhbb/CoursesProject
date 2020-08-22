ad15f04748b0ade54a7d1960952c46b14ef5e526
bharat <nh.bharathbhushan@iseekworld.in>
2020-08-22 19:35:31 +0530

    Updates

---
+++ precache-manifest.0b862accb0ddf39187a40472f5260457.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57807354be2cc052786be56fcf7854ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e4085fe7c4d3be2195a",
+    "url": "/static/css/main.90bc6fb6.chunk.css"
+  },
+  {
+    "revision": "3911c6f9fd6d04a17553",
+    "url": "/static/js/2.cc9dcc2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc9dcc2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4085fe7c4d3be2195a",
+    "url": "/static/js/main.3556ee8c.chunk.js"
+  },
+  {
+    "revision": "faaae247f2136725277e",
+    "url": "/static/js/runtime-main.dd85382f.js"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "4aaf92d0ed86d1b1165581081977af0d",
+    "url": "/static/media/open-menu.4aaf92d0.svg"
+  }
+]);

d5a8935b8087acc441df95f76fc7ae38e41c1880
bharat <nh.bharathbhushan@iseekworld.in>
2020-08-22 19:55:29 +0530

    Updates

---
+++ precache-manifest.ad997b7115b4a977a32e397bc49c6c57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08bb5eb5aa03f2f673be7434fc057d3e",
+    "url": "/CoursesProject/index.html"
+  },
+  {
+    "revision": "197848e9c16ae61295e2",
+    "url": "/CoursesProject/static/css/main.90bc6fb6.chunk.css"
+  },
+  {
+    "revision": "3911c6f9fd6d04a17553",
+    "url": "/CoursesProject/static/js/2.cc9dcc2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoursesProject/static/js/2.cc9dcc2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197848e9c16ae61295e2",
+    "url": "/CoursesProject/static/js/main.3bead291.chunk.js"
+  },
+  {
+    "revision": "29181a349f844d47ae00",
+    "url": "/CoursesProject/static/js/runtime-main.546b9e3a.js"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/CoursesProject/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "4aaf92d0ed86d1b1165581081977af0d",
+    "url": "/CoursesProject/static/media/open-menu.4aaf92d0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9c952f9ac9436d44a8bcfc48ae9529b.js"
+  "/CoursesProject/precache-manifest.ad997b7115b4a977a32e397bc49c6c57.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CoursesProject/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
